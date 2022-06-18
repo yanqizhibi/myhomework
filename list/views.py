07ccd38f04521aa91d1cc0557ae0ce40fd4eb386
yanqizhibi <71682071+yanqizhibi@users.noreply.github.com>
2022-06-18 15:13:09 +0800

    first commit

---
+++ list/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from list.models import Item, List
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+def home_page(request):
+    return render(request, 'home.html')
+
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    return render(request, 'list.html', {'list': list_})
+
+def new_list(request):
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/list/{list_.id}/')
+
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/list/{list_.id}/')
